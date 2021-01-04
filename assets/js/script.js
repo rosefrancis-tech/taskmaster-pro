b28caef17b18db95985f30d9d287a172c2a79b58
rosefrancis-tech <32376285+rosefrancis-tech@users.noreply.github.com>
2021-01-04 12:00:25 -0500

    make date editable on click

--- assets/js/script.js
+++ assets/js/script.js
@@ -89,7 +89,25 @@ $(".list-group").on("blur", "textarea", function() {
   $(this).replaceWith(taskP);
 });
 
+// due date was clicked
+$(".list-group").on("click", "span", function() {
+  // get current text
+  var date = $(this)
+    .text()
+    .trim();
 
+  // create new input element
+  var dateInput = $("<input>")
+    .attr("type", "text")
+    .addClass("form-control")
+    .val(date);
+
+  // swap out elements
+  $(this).replaceWith(dateInput);
+
+  // automatically focus on new element
+  dateInput.trigger("focus");
+});
 
 // modal was triggered
 $("#task-form-modal").on("show.bs.modal", function() {
