661679ddc4f850fe97fbbdf4bab2719107ec70d7
rosefrancis-tech <32376285+rosefrancis-tech@users.noreply.github.com>
2021-01-04 11:51:57 -0500

    save edited task

--- assets/js/script.js
+++ assets/js/script.js
@@ -45,6 +45,7 @@ var saveTasks = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 };
 
+// event listerner for editing
 $(".list-group").on("click", "p", function() {
   var text = $(this)
     .text()
@@ -56,6 +57,28 @@ $(".list-group").on("click", "p", function() {
   
   $(this).replaceWith(textInput);
   textInput.trigger("focus");
+});
+
+// event listener for saving edited tasks
+$(".list-group").on("blur", "textarea", function() {
+  // get the textarea's current value/text
+  var text = $(this)
+  .val()
+  .trim();
+
+  // get the parent ul's id attribute
+  var status = $(this)
+  .closest(".list-group")
+  .attr("id")
+  .replace("list-", "");
+
+  // get the task's position in the list of other li elements
+  var index = $(this)
+  .closest(".list-group-item")
+  .index();
+
+  tasks[status][index].text = text;
+  saveTasks();
 });
 
 
