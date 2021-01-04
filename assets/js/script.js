dfda3e84248f03cfe143d9f85d0fa194d1fcabd9
rosefrancis-tech <32376285+rosefrancis-tech@users.noreply.github.com>
2021-01-04 10:59:09 -0500

    edit task

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,7 +33,7 @@ var loadTasks = function() {
 
   // loop over object properties
   $.each(tasks, function(list, arr) {
-    console.log(list, arr);
+    //console.log(list, arr);
     // then loop over sub-array
     arr.forEach(function(task) {
       createTask(task.text, task.date, list);
@@ -45,6 +45,18 @@ var saveTasks = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 };
 
+$(".list-group").on("click", "p", function() {
+  var text = $(this)
+    .text()
+    .trim();
+  
+  var textInput = $("<textarea>")
+    .addClass("form-control")
+    .val(text);
+  
+  $(this).replaceWith(textInput);
+  textInput.trigger("focus");
+});
 
 
 
