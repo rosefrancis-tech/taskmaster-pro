97dc1072a80909248e3685808e29b9fef91f19b1
rosefrancis-tech <32376285+rosefrancis-tech@users.noreply.github.com>
2021-01-09 02:35:36 -0500

    add background colors for overdue and due-soon

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,6 +13,10 @@ var createTask = function(taskText, taskDate, taskList) {
   // append span and p element to parent li
   taskLi.append(taskSpan, taskP);
 
+    // check due date
+    auditTask(taskLi);
+
+
   // append to ul list on the page
   $("#list-" + taskList).append(taskLi);
 };
@@ -255,5 +259,24 @@ $("#remove-tasks").on("click", function() {
 $("#modalDueDate").datepicker({
   minDate: 1
 });
+
+var auditTask = function(taskEl) {
+  // get date from task element
+  var date = $(taskEl).find("span").text().trim();
+
+  // convert to moment object at 5:00pm
+  var time = moment(date, "L").set("hour", 17);
+
+  // remove any old classes from element
+  $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
+
+  // apply new class if task is near/over due date
+  if (moment().isAfter(time)) {
+    $(taskEl).addClass("list-group-item-danger");
+  } 
+  else if (Math.abs(moment().diff(time, "days")) <= 2) {
+    $(taskEl).addClass("list-group-item-warning");
+  }
+};
 // load tasks for the first time
 loadTasks();
